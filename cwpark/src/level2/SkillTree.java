962269822e0cfe991689466b9b8d50bf9fa9331b
cwJohnPark <pierrotcw@gmail.com>
2020-11-25 23:41:49 +0900

    [Day1] SkillTree - another solution

--- cwpark/src/level2/SkillTree.java
+++ cwpark/src/level2/SkillTree.java
@@ -41,5 +41,37 @@ class Solution {
         }
         return answer;
     }
+}
 
+// map 없이 푸는 방법
+class SolutionAnother {
+    public int solution(String skill, String[] skill_trees) {
+
+        int answer = 0;
+
+        for (String skillTree : skill_trees) {
+            int learningIdx = 0;
+
+            boolean isAble = true;
+
+            for (char curSkill : skillTree.toCharArray()) {
+
+                int skillIdx = skill.indexOf(curSkill);
+
+                if (skillIdx == -1)
+                    continue;
+                else if (skillIdx == learningIdx)
+                    learningIdx++;
+                else {
+                    isAble = false;
+                    break;
+                }
+            }
+
+            if (isAble)
+                answer++;
+        }
+
+        return answer;
+    }
 }
