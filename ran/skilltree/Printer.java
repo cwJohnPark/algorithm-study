1d100b78d7fe3927d72ccb90fbc611946d3b7bce
doll6777 <doll6777@nate.com>
2020-11-29 18:01:10 +0900

    update printer

---
+++ ran/skilltree/Printer.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        
+        List<Integer> sortedPriorities = new ArrayList<>();
+        List<Integer> priorityQueue = new ArrayList<>();
+    
+        for (int i = 0; i < priorities.length; i++) {
+            sortedPriorities.add(priorities[i]);
+            priorityQueue.add(priorities[i]);
+        }
+        sortedPriorities.sort(Comparator.reverseOrder());
+        
+        int answer = 0;
+        int queueIndex = 0;
+    
+        while (!sortedPriorities.isEmpty()) {
+            int queueValue = priorityQueue.get(queueIndex);
+            int sortedValue = sortedPriorities.get(0);
+            
+            if(sortedValue == queueValue) {
+                answer++;
+                
+                if(queueIndex == location) {
+                    break;
+                }
+                sortedPriorities.remove(0);
+            }
+            queueIndex = (queueIndex + 1) % priorityQueue.size();
+        }
+        
+        return answer;
+    }
+}
